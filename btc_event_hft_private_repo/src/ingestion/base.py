fcce3a562124bab3437ff4af7b96547a28e59bb8
shellingfordh <107742516+Shellingfordh@users.noreply.github.com>
2026-09-05 02:17:14 -0400

    Delete btc_event_hft_private_repo directory

--- btc_event_hft_private_repo/src/ingestion/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from collections.abc import Iterator
-from src.core.models import Event
-
-class EventSource(ABC):
-    @abstractmethod
-    def events(self) -> Iterator[Event]:
-        raise NotImplementedError
